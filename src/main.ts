5dabf507db370dc3387097d62fd960b0b3027024
DimaGrimach <chiki.briki2@gmail.com>
2024-07-17 17:15:02 +0300

    dev main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+!(async function bootstrap(): Promise<void> {
+   const app = await NestFactory.create(AppModule, {
+      logger: ['error', 'warn', 'log'],
+   });
+
+   // Define the CORS options
+   const corsOptions: CorsOptions = {
+      origin: [
+         process.env.CORS_HOST_HTTP || 'http://localhost:3003',
+      ],
+      methods: 'POST,GET,PATCH,DELETE',
+      credentials: true, // Enable cookies and authentication headers
+   };
+
+   app.enableCors(corsOptions);
+   app.setGlobalPrefix('api');
+
+   await app.listen(+(process.env.NODE_LOCAL_PORT || 3008));
+})();
