21efcc777bf8c7973738b0a7a2718addce699569
DimaGrimach <chiki.briki2@gmail.com>
2024-08-12 18:57:47 +0300

    pure WebSocket beckend

--- src/posts/notifications.gateway.ts
+++ src/posts/notifications.gateway.ts
@@ -1,24 +1,32 @@
 import {
-   ConnectedSocket,
-   MessageBody, OnGatewayConnection,
-   WebSocketGateway
+   WebSocketGateway,
+   OnGatewayInit,
+   OnGatewayConnection,
+   OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import {Logger} from '@nestjs/common';
+import {Server, WebSocket} from 'ws';
 
-@WebSocketGateway(3007, {
-   cors: {origin: "*"},
-   transports: ['websocket'],
-})
-export class NotificationsGateway implements OnGatewayConnection {
-   async handleConnection(@MessageBody() data: any, @ConnectedSocket() client: any): Promise<string> {
-      console.log('handleEvent-', data);
-      console.log('client-', client);
-      return data;
+@WebSocketGateway(3007, {cors: {origin: '*'}})
+export class NotificationsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+   private logger: Logger = new Logger('NotificationsGateway');
+   private wss: Server;
+
+   afterInit(server: Server) {
+      this.wss = server;
+       this.logger.log('Init');
    }
 
-   /*  @SubscribeMessage('events')
-     handleEvent(@MessageBody() data: any, @ConnectedSocket() client: Socket,): string {
-        console.log('handleEvent-', data);
-        return data;
-     }*/
+   handleConnection(client: WebSocket) {
+      this.logger.log(`Client Connected: ${client}`);
+      client.on('message', (message) => {
+         this.logger.log(`Received message: ${message}`);
+         // Echo the message back to the client
+         client.send(message);
+      });
+   }
 
+   handleDisconnect(client: WebSocket) {
+      this.logger.log(`Client disconnected: ${client}`);
+   }
 }
